64c410cbe89a2e26db4868aa28642075a83d7459
Dan <daniel_mark34@yahoo.com>
2017-12-12 21:37:24 +0800

    javascript exercise

--- mod05-02/js/script.js
+++ mod05-02/js/script.js
@@ -1,26 +1,29 @@
 function redFunc() {
-	document.getElementById.("red-box").style.backgroundColor = "red";
+	document.getElementById("red-box").style.backgroundColor = "red";
 }
 
 function yellowFunc() {
-	document.getElementById.('yellow-box').style.backgroundColor = "yellow";
+	document.getElementById('yellow-box').style.backgroundColor = "yellow";
 }
 
 function greenFunc() {
-	document.getElementById.('green-box').style.backgroundColor = "green";
+	document.getElementById('green-box').style.backgroundColor = "green";
 }
 
+function orangeFunc() {
+	document.getElementById('orange-box').style.backgroundColor = "orange";
+}
 
 function blueFunc() {
-	document.getElementById.('blue-box').style.backgroundColor = "blue";
+	document.getElementById('blue-box').style.backgroundColor = "blue";
 }
 
 
 function indigoFunc() {
-	document.getElementById.('indigo-box').style.backgroundColor = "indigo";
+	document.getElementById('indigo-box').style.backgroundColor = "indigo";
 }
 
 
 function violetFunc() { 	
-	document.getElementById.('violet-box').style.backgroundColor = "violet";
+	document.getElementById('violet-box').style.backgroundColor = "violet";
 }
