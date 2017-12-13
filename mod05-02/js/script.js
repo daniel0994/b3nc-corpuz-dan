8bd0937b3c0680a6c1b8332b9f8b5c2a736045da
Dan <daniel_mark34@yahoo.com>
2017-12-13 21:27:44 +0800

    javascript

--- mod05-02/js/script.js
+++ mod05-02/js/script.js
@@ -27,3 +27,28 @@ function indigoFunc() {
 function violetFunc() { 	
 	document.getElementById('violet-box').style.backgroundColor = "violet";
 }
+
+
+/*
+*
+*  Dan P. 
+**/
+
+var navClass;
+
+function changeColor(className, id=null) {
+	if (id == null) {
+		navClass = className.replace("nav-box ", ""); /*will get color select from navigation*/
+	} else {
+		var boxClass = className.split(" "); /*will create array of class names*/
+		var boxClassLength = boxClass.length; /*get size of array boxClass*/
+		var element = document.getElementById(id); /*target the container to change the background color*/
+
+		if (boxClassLength > 1) { /*update current bg color*/
+			element.classList.remove(boxClass[1]); /*remove previous bg color*/
+			element.className = boxClass[0] + " " + navClass;
+		} else { /*assign initial background color*/
+			element.className = boxClass + " " + navClass;
+		}
+	}
+}
