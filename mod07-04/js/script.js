f78591ca3410d622ba8191608d0a555eacda2c5c
Dan <daniel_mark34@yahoo.com>
2017-12-14 20:36:48 +0800

    javacsript activity switch case

---
+++ mod07-04/js/script.js
@@ -0,0 +1,46 @@
+function changeBackgroundColor() {
+	var newColor = document.getElementById("newBackgroundColor").value;
+	switch (newColor) {
+		case "red":
+		document.getElementById("mainWrapper").style.backgroundColor = "red";
+		document.getElementById("theMessage").innerHTML = "your new background color is red!";
+		break;
+
+		case "orange":
+		document.getElementById("mainWrapper").style.backgroundColor = "orange";
+		document.getElementById("theMessage").innerHTML = "your new background color is red!";
+		break;
+
+		case "yellow":
+		document.getElementById("mainWrapper").style.backgroundColor = "yellow";
+		document.getElementById("theMessage").innerHTML = "your new background color is red!";
+		break;
+
+		case "green":
+		document.getElementById("mainWrapper").style.backgroundColor = "green";
+		document.getElementById("theMessage").innerHTML = "your new background color is red!";
+		break;
+
+		case "blue":
+		document.getElementById("mainWrapper").style.backgroundColor = "blue";
+		document.getElementById("theMessage").innerHTML = "your new background color is red!";
+		break;
+
+		case "indigo":
+		document.getElementById("mainWrapper").style.backgroundColor = "indigo";
+		document.getElementById("theMessage").innerHTML = "your new background color is red!";
+		break;
+
+		case "violet":
+		document.getElementById("mainWrapper").style.backgroundColor = "violet";
+		document.getElementById("theMessage").innerHTML = "your new background color is red!";
+		break;
+	
+	default:
+		break;
+
+	}
+
+
+	
+}
